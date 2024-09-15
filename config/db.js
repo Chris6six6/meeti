e098a315395d566a0540ab646e12057e151158f3
Chris666 <g.a_cristo42@hotmail.com>
2024-09-15 15:24:03 -0600

    Primera version terminada rende PSQL

--- config/db.js
+++ config/db.js
@@ -1,14 +1,20 @@
 const Sequelize = require('sequelize');
-require('dotenv').config({ path: 'variables.env' })
+require('dotenv').config({ path: 'variables.env' });
 
-module.exports = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {
-    host: process.env.BD_HOST,
-    port: process.env.BD_PORT,
+module.exports = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false, // Opcional: Desactivar el logging en consola si no lo necesitas
+    dialectOptions: {
+        ssl: {
+            require: true, // Si tu base de datos requiere SSL
+            rejectUnauthorized: false // Asegúrate de que SSL no falle debido a un certificado autofirmado
+        }
+    },
     pool: {
         max: 5,
         min: 0,
         acquire: 30000,
         idle: 10000
     }
-})
+});
